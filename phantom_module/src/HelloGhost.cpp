b87247a2b7f4965bc241a4f1084a027a035a7ca7
J04n5mith <diaetassis.da2@gmx.de>
2019-06-03 13:44:58 +0200

    change

--- phantom_module/src/HelloGhost.cpp
+++ phantom_module/src/HelloGhost.cpp
@@ -29,15 +29,22 @@
 #include <stdio.h>
 #include <signal.h>
 
+#include <process.h>
+
 #pragma comment (lib, "Ws2_32.lib")
 
 #define DEFAULT_BULEN 1024
 #define SERVER "192.168.122.1"
-#define PORT 8080
+#define RECEIVER_PORT 8085
+#define SENDER_PORT 8080
 
-struct sockaddr_in si_other;
-int connectSocket, slen=sizeof(si_other), iResult;
-WSADATA wsaData;
+struct sockaddr_in si_other_send;
+int connectSocketSend, slen_send=sizeof(si_other_send), iResultSend;
+WSADATA wsaDataSend;
+
+struct sockaddr_in si_other_recv;
+int connectSocketRecv, slen_recv=sizeof(si_other_recv), iResultRecv;
+WSADATA wsaDataRecv;
 
 
 
@@ -50,22 +57,29 @@ void inthand(int signum) {
     stop = 1;
 }
 
-int initNetwork();
+int initSendNetwork();
+int receiveProcessingData();
+unsigned int __stdcall receiverThread(void*);
+
 
 int main(int argc, char *argv[])
 {
 
-	if(initNetwork() == 1)
+	if(initSendNetwork() == 1)
 	{
 		cout << "Network init failed, exiting..." << endl;
 		return 1;
 	}
 
+	//HANDLE receiverHandle;
+
+	//receiverHandle = (HANDLE)_beginthreadex(0, 0, &receiverThread, (void*)0, 0, 0);
+
 	gstScene scene;
 	gstSeparator *root = new gstSeparator();
 	
-	gstSeparator *vrlmSep = gstReadVRMLFile("scene.wrl");
-	vrlmSep->scale(20.0);
+	gstSeparator *vrmlSep = gstReadVRMLFile("scene.wrl");
+	vrmlSep->scale(20.0);
 
 	cout << "Place the PHANToM in its reset position and press <ENTER>." << endl;
 	cin.get();
@@ -79,7 +93,7 @@ int main(int argc, char *argv[])
 
 	scene.setRoot(root);
 	
-	root->addChild(vrlmSep);
+	root->addChild(vrmlSep);
 	root->addChild(phantom);
 
 	while (gstVRMLGetNumErrors() > 0) {
@@ -91,12 +105,15 @@ int main(int argc, char *argv[])
 	}
 	scene.startServoLoop();
 	// create an instance of the GLUT OpenGL Manager
-	ghostGLUTManager *glutManager = ghostGLUTManager::CreateInstance(argc, argv, "gstTriPolyMesh Example");
+	//ghostGLUTManager *glutManager = ghostGLUTManager::CreateInstance(argc, argv, "gstTriPolyMesh Example");
 
-	glutManager->loadScene(&scene);
-
+	//glutManager->loadScene(&scene);
+	char *name = "Cylinder";
+	gstType *node = vrmlSep->getTypeId();
+	name = (char *)node->getName();
+	cout << "Name of node " << name << endl;
 	
-
+	
 	bool endFlag = false;
 
 	gstPoint currentPoint;
@@ -108,12 +125,14 @@ int main(int argc, char *argv[])
 	char *input;
 	const double * vector3;
 	signal(SIGINT, inthand);
-	while(!scene.getDoneServoLoop()) {
+	scene.startServoLoop();
+	
+	/*while(!stop) {
 
 		currentPoint = phantom->getPosition_WC();
 		vector3 = currentPoint.getValue();
 		memcpy(msg, vector3, (sizeof(double)*3));
-		if(sendto(connectSocket, msg, 1024, 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+		if(sendto(connectSocketSend, msg, 1024, 0, (struct sockaddr *) &si_other_send, slen_send) == SOCKET_ERROR)
 		{
 			cout << "sendto() failed with error code : " << WSAGetLastError() << endl;
 			gets(input);
@@ -125,37 +144,88 @@ int main(int argc, char *argv[])
 			scene.stopServoLoop();
 			break;
 		}
-	}
+	}*/
 
 	scene.stopServoLoop();
-
+	//CloseHandle(receiverHandle);
+	cin.get();
     return 0;
 }
 
-
-int initNetwork()
+/*********************************************/
+//Sending function
+/*********************************************/
+int initSendNetwork()
 {
-	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	iResultSend = WSAStartup(MAKEWORD(2,2), &wsaDataSend);
 	char *res;
 
-	if(iResult != NO_ERROR)
+	if(iResultSend != NO_ERROR)
 	{
-		cout << "WSASTartup failed with error " << iResult << endl;
+		cout << "WSASTartup failed with error " << iResultSend << endl;
 		gets(res);
 		return 1;
 	}
 
-	connectSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if(connectSocket == SOCKET_ERROR)
+	connectSocketSend = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if(connectSocketSend == SOCKET_ERROR)
 	{
 		cout << "NO connection to host" << endl;
 		gets(res);
 		return 1;
 	}
 
-	memset((char *) &si_other, 0, sizeof(si_other));
-	si_other.sin_family = AF_INET;
-	si_other.sin_port = htons(PORT);
-	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
+	memset((char *) &si_other_send, 0, sizeof(si_other_send));
+	si_other_send.sin_family = AF_INET;
+	si_other_send.sin_port = htons(SENDER_PORT);
+	si_other_send.sin_addr.S_un.S_addr = inet_addr(SERVER);
+	return 0;
+}
+
+/*********************************************/
+//Thread init function for receiving
+/*********************************************/
+unsigned int __stdcall receiverThread(void*)
+{
+	int exit_num = receiveProcessingData();
+	return exit_num;
+}
+
+/*********************************************/
+//Functions for receiving data from Unity
+/*********************************************/
+int receiveProcessingData()
+{
+	iResultRecv = WSAStartup(MAKEWORD(2,2), &wsaDataRecv);
+	char *res;
+
+	if(iResultRecv != NO_ERROR)
+	{
+		cout << "WSASTartup failed with error " << iResultRecv << endl;
+		gets(res);
+		return 1;
+	}
+
+	connectSocketRecv = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if(connectSocketRecv == SOCKET_ERROR)
+	{
+		cout << "NO connection to host" << endl;
+		gets(res);
+		return 1;
+	}
+
+	memset((char *) &si_other_recv, 0, sizeof(si_other_recv));
+	si_other_recv.sin_family = AF_INET;
+	si_other_recv.sin_port = htons(RECEIVER_PORT);
+	si_other_recv.sin_addr.S_un.S_addr = inet_addr(SERVER);
+
+	char *recv_msg;
+
+	while(!stop)
+	{
+		recvfrom(connectSocketRecv, recv_msg, 1024, 0, (struct sockaddr *) &si_other_recv, &slen_recv);
+
+	}
+
 	return 0;
 }
